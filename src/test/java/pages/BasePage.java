cd8ca309a0e8c15cad0c6198016c0f622509c483
jovanovmarko <jovanov.s.marko@gmail.com>
2022-10-04 18:22:39 +0200

    added basepage class

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,35 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public abstract class BasePage {
+
+    private WebDriver driver;
+
+    private WebDriverWait driverWait;
+
+    public BasePage() {
+    }
+
+    public BasePage(WebDriver driver, WebDriverWait driverWait) {
+        this.driver = driver;
+        this.driverWait = driverWait;
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public WebDriverWait getDriverWait() {
+        return driverWait;
+    }
+
+    public void setDriverWait(WebDriverWait driverWait) {
+        this.driverWait = driverWait;
+    }
+}
